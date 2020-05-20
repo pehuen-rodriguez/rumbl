a62b7126266b1a9338331fcab3ef43ee492f68a9
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-20 18:44:02 -0300

    small changes

--- lib/rumbl_web/controllers/plugs/auth.ex
+++ lib/rumbl_web/controllers/plugs/auth.ex
@@ -1,7 +1,9 @@
 defmodule RumblWeb.Plugs.Auth do
   import Plug.Conn
+
   import Phoenix.Controller
   import Bcrypt, only: [check_pass: 2, no_user_verify: 0]
+
   alias Rumbl.Accounts
   alias RumblWeb.Router.Helpers, as: Routes
 
@@ -41,7 +43,7 @@ defmodule RumblWeb.Plugs.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
